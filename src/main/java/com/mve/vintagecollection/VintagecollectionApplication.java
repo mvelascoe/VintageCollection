b29b73e87eb751b0f1cc632a0229822477c6cd2a
marivelascoe <maritza.94v@gmail.com>
2024-09-19 14:54:34 -0500

    :tada: initial commit

---
+++ src/main/java/com/mve/vintagecollection/VintagecollectionApplication.java
@@ -0,0 +1,13 @@
+package com.mve.vintagecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintagecollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintagecollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mve/vintagecollection/VintagecollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mve.vintagecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintagecollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
